8d67043da14a5fffb107d0263323aaf3a081e95b
ndevisscher <niek.devisscher@student.uva.nl>
2015-09-04 16:51:23 +0200

    upload App opdracht
    bijn af, alleen nog maar screen 3 en background keuze

--- Huiswerk/Week 1/FirstappWeek1/app/src/main/java/com/devisscher/niek/firstappweek1/SecondScreen.java
+++ Huiswerk/Week 1/FirstappWeek1/app/src/main/java/com/devisscher/niek/firstappweek1/SecondScreen.java
@@ -1,20 +1,68 @@
 package com.devisscher.niek.firstappweek1;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SecondScreen extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second_screen);
+
+        String FullName = getIntent().getStringExtra("InputName");
+        TextView txtDisplay = (TextView)findViewById(R.id.NameImport);
+        txtDisplay.setText("hoi " +FullName);
+
+        Button redbutton = (Button)findViewById(R.id.redbutton);
+        Button bluebutton = (Button)findViewById(R.id.bluebutton);
+        Button greenbutton = (Button)findViewById(R.id.greenbutton);
+
+        redbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondScreen.this, ThirdScreen.class);
+                String ColorChoice = "#FF0000";
+                intent.putExtra("BackgroundColor", ColorChoice);
+                startActivity(intent);
+            }
+        });
+        bluebutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondScreen.this, ThirdScreen.class);
+                String ColorChoice = "#0000FF";
+                intent.putExtra("BackgroundColor", ColorChoice);
+                startActivity(intent);
+            }
+        });
+        greenbutton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SecondScreen.this, ThirdScreen.class);
+                String ColorChoice = "#00FF00";
+                intent.putExtra("BackgroundColor", ColorChoice);
+                startActivity(intent);
+            }
+        });
+
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_second_screen, menu);
         return true;
--- Huiswerk/Week 1/FirstappWeek1/app/src/main/java/com/devisscher/niek/firstappweek1/WelcomeScreen.java
+++ Huiswerk/Week 1/FirstappWeek1/app/src/main/java/com/devisscher/niek/firstappweek1/WelcomeScreen.java
@@ -1,39 +1,63 @@
 package com.devisscher.niek.firstappweek1;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class WelcomeScreen extends Activity {
+public class WelcomeScreen extends Activity
+{
 
-
+    public final static String EXTRA_MESSAGE ="LOOL";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v)
+            {
+                Intent intent = new Intent(WelcomeScreen.this, SecondScreen.class);
+                EditText NameInput = (EditText) findViewById(R.id.NameInput);
+                String FullName = NameInput.getText().toString();
+                intent.putExtra("InputName", FullName);
+                startActivity(intent);
+            }
+        });
     }
 
+
+
+
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_welcome_screen, menu);
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_settings)
+        {
             return true;
         }
 
