f2ded725ac056f52dbe0d098089e11519a698668
ndevisscher <niek.devisscher@student.uva.nl>
2015-09-25 00:21:41 +0200

    update lists
    alleen het removen nog, longclick werkt al

--- Huiswerk/Week 4/todolist/app/src/main/java/com/example/niek/todolist/MainActivity.java
+++ Huiswerk/Week 4/todolist/app/src/main/java/com/example/niek/todolist/MainActivity.java
@@ -5,11 +5,15 @@ import java.util.Arrays;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -25,16 +29,25 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         // Find the ListView resource.
         ListView = (ListView) findViewById( R.id.listView );
+        ListView.setLongClickable(true);
+        ListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            public boolean onItemLongClick(AdapterView<?> arg0, View v, int index, long arg3) {
+
+                Log.v("long clicked", "pos: " + index);
+                ListAdapter.remove([index]);
+                ArrayAdapter.notifyDataSetChanged();
+                return false;
+            }
+        });
 
         // Create and populate the List
         String[] todo = new String[] {""};
         ArrayList<String> todolist = new ArrayList<String>();
         todolist.addAll(Arrays.asList(todo));
-
-        // Create ArrayAdapter using the list.
-        listAdapter = new ArrayAdapter<String>(this, R.layout.rows, todolist);
 
         // Create ArrayAdapter using the list.
         listAdapter = new ArrayAdapter<String>(this, R.layout.rows, todolist);
