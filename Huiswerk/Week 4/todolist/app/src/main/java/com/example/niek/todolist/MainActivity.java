d2cf3dfbccfad757fbe02db6f18a997bac8bee6a
ndevisscher <niek.devisscher@student.uva.nl>
2015-09-25 00:38:04 +0200

    Last edit to listview app
    the app is now ready for grading

--- Huiswerk/Week 4/todolist/app/src/main/java/com/example/niek/todolist/MainActivity.java
+++ Huiswerk/Week 4/todolist/app/src/main/java/com/example/niek/todolist/MainActivity.java
@@ -5,39 +5,44 @@ import java.util.Arrays;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
 
+    // Creating the variables to be used for our ListView
     private ListView ListView ;
     private ArrayAdapter<String> listAdapter ;
 
     String word;
 
-    /** Called when the activity is first created. */
+    // set the function to be executed on activity start
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create the List as blank list
+        String[] todo = new String[] {""};
+        ArrayList<String> todolist = new ArrayList<String>();
+        todolist.addAll(Arrays.asList(todo));
 
-        // Find the ListView resource.
+        // Create new ArrayAdapter listAddapter using the list
+        listAdapter = new ArrayAdapter<String>(this, R.layout.rows, todolist);
+
+        // Find the ListView in the layout
         ListView = (ListView) findViewById( R.id.listView );
         ListView.setLongClickable(true);
+        // Setting the longlcick to remove the chosen item
         ListView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
             public boolean onItemLongClick(AdapterView<?> arg0, View v, int index, long arg3) {
 
-                Log.v("long clicked", "pos: " + index);
                 String remove = listAdapter.getItem(index);
                 listAdapter.remove(remove);
                 listAdapter.notifyDataSetChanged();
@@ -45,18 +50,13 @@ public class MainActivity extends Activity {
             }
         });
 
-        // Create and populate the List
-        String[] todo = new String[] {""};
-        ArrayList<String> todolist = new ArrayList<String>();
-        todolist.addAll(Arrays.asList(todo));
-
-        // Create ArrayAdapter using the list.
-        listAdapter = new ArrayAdapter<String>(this, R.layout.rows, todolist);
-
-        // Create the input registration
+        // Getting the button for input registration
         final Button input = (Button)findViewById(R.id.button);
+        // Collecting the word in editText
         final EditText wordGet = (EditText) findViewById(R.id.editText);
+        // The clickListener to know when a word needs to be added to the list
         input.setOnClickListener(new View.OnClickListener() {
+            // Adding the word to the list and emptying the input field
             @Override
             public void onClick(View v) {
                 word = wordGet.getText().toString();
@@ -65,7 +65,7 @@ public class MainActivity extends Activity {
             }
         });
 
-        // Set the ArrayAdapter as the ListView's adapter.
+        // Set addapter for ListView as listAdapter
         ListView.setAdapter( listAdapter );
     }
 }
